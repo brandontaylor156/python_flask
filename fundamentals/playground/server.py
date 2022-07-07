cb1ff94c1fed660ae849df22892dd0920d291b91
brandontaylor156 <brandon.david4@outlook.com>
2022-07-07 01:05:03 -0700

    3 different functions

--- fundamentals/playground/server.py
+++ fundamentals/playground/server.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/play/')
+def index():
+    return render_template("index.html", color='blue', times=3)
+
+@app.route('/play/<int:times>/')
+def index2(times):
+    return render_template("index.html", color='blue', times=times)
+
 @app.route('/play/<int:times>/<string:color>/')
-def index(times, color):
+def index3(times, color):
     return render_template("index.html", color=color, times=times)	# notice the 2 new named arguments!
 
 if __name__=="__main__":
